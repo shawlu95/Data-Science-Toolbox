f070126754dffdf5155e0b3a85c6614aa99a63ac
Shaw <shawlu@github.com>
2019-05-05 14:43:47 -0700

    Updated binary search readme.

--- algorithm/BinarySearch/278_First_Bad_Version.py
+++ algorithm/BinarySearch/278_First_Bad_Version.py
@@ -10,10 +10,40 @@ class Solution(object):
         :rtype: int
         """
         l, r = 0, n
-        while l != r:
+        while l < r: # cannot have equal sign, because r = m may get stuck
             m = (l + r) // 2
             if isBadVersion(m):
                 r = m
             else:
                 l = m + 1
+        return l
+
+# plain bsearch
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        each middle point can be GG, GB or BB
+        GG: l <- m + 1
+        GB: return m
+        BB: r <- m - 1
+        """
+        l, r = 1, n
+        while l <= r:
+
+            m = (l + r) // 2
+            if not isBadVersion(m) and isBadVersion(m + 1):
+                # return first bad version, not last good version
+                return m + 1
+            elif not isBadVersion(m) and not isBadVersion(m + 1):
+                # two good, search right
+                l = m + 1
+            elif isBadVersion(m) and isBadVersion(m + 1):
+                # two bad, search left
+                r = m - 1
+
+        # edge case:
+        # first version bad: 10, 1
+        # last version bad: 10, 10
         return l
---
+++ algorithm/BinarySearch/readme.md
@@ -0,0 +1,7 @@
+### Basic
+* Plain vanilla binary search while loop [704](704_Binary_Search.py)
+* Plain vanilla binary search recursion [457](457_Classical_Binary_Search.py)
+* Plain vanilla binary search for insert position [35](35_Search_Insert_Position.py)
+
+### Application
+* Find point of change (first occurrence of a new series) [278](278_First_Bad_Version.py)

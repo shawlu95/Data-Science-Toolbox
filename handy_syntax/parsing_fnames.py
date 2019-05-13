145649824893c8455461615b363ec767f457c8ca
Shaw <shawlu@github.com>
2019-05-13 01:04:55 -0700

    Updated gitignore.

--- handy_syntax/parsing_fnames.py
+++ handy_syntax/parsing_fnames.py
@@ -1,6 +1,6 @@
 import sys
 
-p = lambda s: s.replace(" ", "_").replace("-", "_").lower()
+p = lambda s: s.replace(" ", "_").replace("-", "_").replace("-", "").replace(":", "").lower()
 
 for s in sys.argv[1:]:
 	p(s)

b44df81f8fa6d756a0f2653088cc5bd9d4088dce
Shaw <shawlu@github.com>
2019-05-14 12:38:48 -0700

    Update wood_cut.py

--- algorithm/binary_search/wood_cut.py
+++ algorithm/binary_search/wood_cut.py
@@ -1,3 +1,22 @@
+"""
+[LintCode] 183 Wood Cut
+Description
+Given n pieces of wood with length L[i] (integer array). Cut them into small pieces to guarantee you could have equal or more than k pieces with the same length. What is the longest length you can get from the n pieces of wood? Given L & k, return the maximum length of the small pieces.
+
+Notice
+You couldn't cut wood into float length.
+
+If you couldn't get >= k pieces, return 0.
+
+
+Example
+For L=[232, 124, 456], k=7, return 114.
+
+
+Challenge
+O(n log Len), where Len is the longest length of the wood.
+"""
+
 def woodCut(arr, k):
     """
     arr: array of integer denoting lengths of wood

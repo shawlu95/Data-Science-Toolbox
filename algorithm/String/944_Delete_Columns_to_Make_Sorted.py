c9f50d55fd26e23e1c1ecf2d977f5ebe00b8d66a
Shaw <shawlu@github.com>
2019-05-19 13:00:53 -0700

    Removed algorithm folder to a separate repository.

--- algorithm/String/944_Delete_Columns_to_Make_Sorted.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def minDeletionSize(self, A: List[str]) -> int:
-        ans = []
-        for i in range(len(A[0])):
-            for j in range(len(A) - 1):
-                if A[j][i] > A[j + 1][i]:
-                    ans.append(i)
-                    break
-        return len(ans)
--- algorithm/syntax/Filter.py
+++
@@ -1,5 +0,0 @@
-lis = list(range(-3, 3, 1))
-print(lis)
-
-result = list(filter(lambda x : x > 0, lis))
-print(result)
